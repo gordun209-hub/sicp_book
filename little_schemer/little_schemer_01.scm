61f00375ee1459e85ccea9b6dab892db725f82a7
gordun209 <ali-han80@hotmail.com>
2022-06-26 19:51:26 +0300

    aa

--- little_schemer/little_schemer_01.scm
+++ little_schemer/little_schemer_01.scm
@@ -22,3 +22,53 @@
 (cons '((help) this) '(is very ((hard) to learn)))
 
 (cons 'a '()) ; (a)
+
+; The Laws of cons
+; The primitive cons takes two arguments.
+; the second argument to cons must be a list
+; the result is a list
+
+; The law of Null?
+; the primitive null? is defined only for lists
+
+; the law of Eq?
+; the primitive eq? takes two arguments. each must be a non numeric atom.
+(define l '(Marry had a little lamb chop))
+(define a 'Marry)
+(eq? (car l) a) ; #t
+
+(define lat?
+  (lambda (l)
+    (cond
+      [(null? l) #t] ; check if null
+      ; if not null take first element and check if atom if not
+      ; call same function with cdr l which drops first and takes rest
+      [(atom?  (car l)) (lat? (cdr l))]
+      [else #f])))
+
+(lat? '(bacon and eggs))
+(define member?
+  (lambda (a lat)
+    (cond
+      ; check if nulll if null return false
+      [(null? lat) #f]
+      ; check if either first atom of lat and given a is equal
+      ; of not call member function with same a but cdr lat
+      [else (or (eq? (car lat) a) (member? a (cdr lat)))])))
+(member? 'meat '(mashed potatoes and meat gravy))
+
+
+; The first commandment
+; Always ask null? as the first question in expressing any function
+
+
+
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+       (else (cond
+               ((eq? (car lat) a) (cdr lat))
+                (else (rember a
+                              (cdr lat))))))))
+
