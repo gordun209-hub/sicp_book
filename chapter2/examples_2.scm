7073b66187ca06ceb40ad7ff270079f65269249b
gordun209 <ali-han80@hotmail.com>
2022-07-08 14:23:11 +0300

    chapter 2 cont

---
+++ chapter2/examples_2.scm
@@ -0,0 +1,152 @@
+#lang sicp
+
+
+(define (same-parity first . rest)
+  (let ((yes? (if (even? first)
+                  even?
+                  odd?)))
+    (define (iter items result)
+      (if (null? items)
+          (reverse result)
+          (iter (cdr items)
+                (if (yes? (car items))
+                    (cons (car items) result)
+                    result))))
+    (iter rest (list first))))
+
+(define (same-parity3 first . rest)
+  (let ((yes? (if (even? first)
+                  even?
+                  odd?)))
+    (define (iter result items)
+      (if (null? items) result)
+      (iter (if (yes? (car items))
+                (cons (car items) result)
+                result)
+            (cdr items)))
+    (iter 'first rest)))
+
+
+
+
+(same-parity 2 4 5 6 7)
+
+(define (sam-parity first . rest)
+  (define (inter yes? lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((yes? (car lat)) (cons (car lat) (inter yes? (cdr lat))))
+      (else
+       (inter yes? (cdr lat)))))
+  (if (odd? first)
+      (inter odd? rest)
+      (inter even? rest)))
+
+(define (same-parity-2 first . rest)
+  (define (congruent-to-first-mod-2? a)
+    (= (remainder a 2) (remainder first 2)))
+
+  (define (select-same-parity items)
+    (if (null? items)
+        items
+        (let ((curr (car items))
+              (select-rest (select-same-parity (cdr items))))
+          (if (congruent-to-first-mod-2? curr)
+              (cons curr select-rest)
+              select-rest))))
+  (cons first (select-same-parity rest)))
+
+
+
+;; an alternative implementation by andras:
+(define (same-parity-andras a . l)
+  (define (sp-builder result tail)
+    (if (null? tail)
+        result
+        (if (even? (+ a (car tail)))
+            ;;test for same parity
+            ;;if the current beginning of the rest (car tail) is the sa
+            ;me parity as "a", then it is appended to the result, else the result is left untouched
+            (sp-builder (append result (list (car tail))) (cdr tail))
+            (sp-builder result (cdr tail)))))
+  (sp-builder (list a) l))
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+
+((lambda (x) (* x x)) 2)
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (* (car items) (car items)) (square-list (cdr items)))))
+
+(square-list (list 2 3 4))
+
+(define suqare-with-lambda
+  (lambda (items)
+    (map (lambda (x) (* x x)) items)))
+(suqare-with-lambda (list 2 3 4))
+
+(define (square x) (* x x))
+
+(define (square-list-2 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (square (car things))
+                    (cdr (answer))))))
+  (iter items nil))
+
+(square-list-2 '(5 2 4))
+
+; (iter '(5 2 4) nil)
+; (iter (2 4) (cons (square (5)) nil))
+; (iter (2 4) (25)
+(cons (square (car '(5 2 4))) nil) ; => 25
+; (iter (4) (cons (square 2) (25)))
+(append (list 25) (square (car (list 2 3))))
+;(cons (square 2) '(25)) ; => (4 25)
+
+
+(define (square-list-23 items)
+  (define (iter l pick)
+    (define r (square (car l)))
+    (if (null? (cdr l))
+        (pick (list r))
+        (iter (cdr l) (lambda (x) (pick (cons r x))))))
+  (iter items (lambda (x) x)))
+
+
+(define (reverse items)
+  (define (iter items result)
+    (if (null? items)
+        result
+        (iter (cdr items) (cons (car items) result))))
+  (iter items nil))
+
+(define (different-reverse items)
+  (if (null? (cdr items))
+      items
+      (append (reverse (cdr items))
+              (cons (car items) nil))))
+
+
+(define (count-leaves x)
+  (cond
+    ((null? x) 0)
+    ((not (pair? x)) 1)
+    (else (+
+           (count-leaves (car x))
+           (count-leaves (cdr x))))))
+
--- chapter2/sicp_2_exercises.scm
+++ chapter2/sicp_2_exercises.scm
@@ -379,3 +379,86 @@
 
 (subsets '(1 2 3))
 
+
+(define square
+  (lambda (x)
+    (* x x)))
+
+
+;; (define (sum-odd-squares tree)
+;;   (cond ((null? tree) 0)
+;;         ((not (pair? tree))
+;;          (if (odd? tree) (square tree) 0))
+;;         (else (+ (sum-odd-squares (car tree))
+;;                  (sum-odd-squares (cdr tree))))))
+
+;; (define (even-fibs n )
+;;   (define (next k)
+;;     (if (> k n)
+;;         nil
+;;         (let ((f (fib k)))
+;;           (if (even? f)
+;;               (cons f (next (+ k 1)))
+;;               (next (+ k 1))))))
+;;   (next 0 ))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(enumerate-interval 2 7)
+
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate
+   + 0 (map square (filter odd? (enumerate-tree tree)))))
+
+
+;; (define (even-fibs n)
+;;   (accumulate
+;;     cons
+;;     nil
+;;     (filter even? (map fib (enumerate-interval 0 n)))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define prime?
+  (lambda (x)
+    (= 1 (remainder  x 2))))
+
+(prime? 5)
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define a 1)
+(define b 2)
+
+;(list a b) => (1 2)
+;(list 'a 'b) => (a b)
+;(list 'a b) => (a 2)
+
+(car ''abracadabra)
+
